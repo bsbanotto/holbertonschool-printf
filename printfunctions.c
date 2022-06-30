ea150d867b6c322086274730b259890b3e7f05bd
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-30 09:32:10 -0700

    Some functions called by _printf.c, starting testing for task 0

---
+++ printfunctions.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
+
+/**
+ * print_c - writes the character c to stdout via _putchar
+ * @c: The character to print
+ *
+ * Return: number of characters printed (should be 1)
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_c(va_list c)
+{
+	unsigned int count = 0;
+
+	if(!c)
+		return (0);
+
+	_putchar((char)va_arg(c, int));
+	count++;
+
+	return (count);
+}
+
+/**
+ * print_s - writes the string s to stdout via _putchar
+ * @s: The string to print
+ *
+ * Return: On success, the length of string. 
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int print_s(va_list s)
+{
+	unsigned int length;
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = (null);
+
+	for (length = 0; str[length]; length++)
+	{
+		_putchar(str[length];
+	}
+
+	return (length);
+}
+
+/**
+ * print_di - writes the integer di to stdout
+ * @di: The integer to print out
+ *
+ * Return: integer length count on success.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int print_di(va_list di)
+{
+	unsigned int count = 0;
+
+	return (count);
+}
