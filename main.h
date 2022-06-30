058bd63b32b7a0f5c7b85526d7fdcadd5c508815
AaronManuel15 <aaronmanuel15@gmail.com>
2022-06-30 08:01:54 -0700

    Header file for _printf project

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_di(va_list di);
+
+
+typedef struct printformat
+{
+	char *spec;
+	void (*f)(va_list)
+} format_type;
+
+#endif
